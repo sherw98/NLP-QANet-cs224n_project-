0f76a3392c3bee36aef1fc9190084961207220b7
Alex Fan <alexfan@stanford.edu>
2022-02-23 22:36:41 -0800

    fix

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ import util
 from args import get_train_args
 from collections import OrderedDict
 from json import dumps
-from models import BiDAF, BiDAF_character
+from models import BiDAF, BiDAF_character, QANet
 from tensorboardX import SummaryWriter
 from tqdm import tqdm
 from ujson import load as json_load
